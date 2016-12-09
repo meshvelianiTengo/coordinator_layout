aec9480fbbc9acafb18a294356ad60dd750636d4
Tengo Meshveliani <meshvelianitengo@gmail.com>
2016-12-09 15:22:27 +0400

    no message

---
+++ CoordinatorLayout/app/src/main/java/com/voli/coordinatorlayout/behaviors/AvatarImageBehavior.kt
@@ -0,0 +1,59 @@
+package com.voli.coordinatorlayout.behaviors
+
+import android.support.design.widget.CoordinatorLayout
+import android.support.v7.widget.Toolbar
+import android.view.View
+import android.widget.ImageView
+
+/**
+ * Created by tengo on 12/9/16.
+ */
+class AvatarImageBehavior : CoordinatorLayout.Behavior<ImageView>(){
+
+    override fun layoutDependsOn(parent: CoordinatorLayout?, child: ImageView?, dependency: View?): Boolean {
+        return dependency is Toolbar
+    }
+
+    override fun onDependentViewChanged(parent: CoordinatorLayout?, child: ImageView?, dependency: View?): Boolean {
+
+        return super.onDependentViewChanged(parent, child, dependency)
+    }
+
+    /*
+    maybeInitProperties(child, dependency);
+
+        final int maxScrollDistance = (int) (mStartToolbarPosition);
+        float expandedPercentageFactor = dependency.getY() / maxScrollDistance;
+
+        if (expandedPercentageFactor < mChangeBehaviorPoint) {
+            float heightFactor = (mChangeBehaviorPoint - expandedPercentageFactor) / mChangeBehaviorPoint;
+
+            float distanceXToSubtract = ((mStartXPosition - mFinalXPosition)
+                    * heightFactor) + (child.getHeight()/2);
+            float distanceYToSubtract = ((mStartYPosition - mFinalYPosition)
+                    * (1f - expandedPercentageFactor)) + (child.getHeight()/2);
+
+            child.setX(mStartXPosition - distanceXToSubtract);
+            child.setY(mStartYPosition - distanceYToSubtract);
+
+            float heightToSubtract = ((mStartHeight - mCustomFinalHeight) * heightFactor);
+
+            CoordinatorLayout.LayoutParams lp = (CoordinatorLayout.LayoutParams) child.getLayoutParams();
+            lp.width = (int) (mStartHeight - heightToSubtract);
+            lp.height = (int) (mStartHeight - heightToSubtract);
+            child.setLayoutParams(lp);
+        } else {
+            float distanceYToSubtract = ((mStartYPosition - mFinalYPosition)
+                    * (1f - expandedPercentageFactor)) + (mStartHeight/2);
+
+            child.setX(mStartXPosition - child.getWidth()/2);
+            child.setY(mStartYPosition - distanceYToSubtract);
+
+            CoordinatorLayout.LayoutParams lp = (CoordinatorLayout.LayoutParams) child.getLayoutParams();
+            lp.width = (int) (mStartHeight);
+            lp.height = (int) (mStartHeight);
+            child.setLayoutParams(lp);
+        }
+     */
+
+}
